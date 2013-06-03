e578750a23f3c7438a69320f8e5ac0c8f86d5d39
jinchizhong <jinchizhong@kingsoft.com>
2013-06-03 11:31:48 +0800

    add other user/group/root setup support

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# setup.sh will load config/setup.conf to get setup info
 
 if [ "x$USER" != "xroot" ]; then
 	sudo ./$0 "$@"
@@ -13,8 +14,18 @@ function die()
 	exit 1
 }
 
+function config_file()
+{
+	sed "s/@USER@/${x_user}/g; s/@GROUP@/${x_group}/g; s/@ROOT@/${x_root}/g;" < "$1" > "$2"
+}
+
+x_user="www-data"
+x_group="www-data"
+x_root="/var/www"
+source config/setup.conf
+
 # check root directory
-[ "x$(pwd)" == "x/var/www" ] || die "website must be installed to /var/www"
+[ "x$(pwd)" == "x${x_root}" ] || die "website must be installed to ${x_root}"
 
 # check lighttpd
 which lighttpd || die "can not found lighttpd"
@@ -25,7 +36,8 @@ update-rc.d lighttpd disable
 
 # create own serve
 update-rc.d -f lighttpd-wps-community remove 
-cp "$(pwd)/setup/lighttpd.init" "/etc/init.d/lighttpd-wps-community"
+config_file "setup/lighttpd.init" "/etc/init.d/lighttpd-wps-community"
+config_file "setup/lighttpd.conf" "config/lighttpd.conf"
 [ -d "log" ] || mkdir log
 update-rc.d lighttpd-wps-community start 09 2 3 4 5 . stop 09 0 1 6 .
 update-rc.d lighttpd-wps-community enable
