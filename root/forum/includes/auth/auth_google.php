45cc188364746805ed8295127a364a213a5e3bba
jinchizhong <jinchizhong@kingsoft.com>
2013-06-18 13:43:14 +0800

    add google login support

---
+++ root/forum/includes/auth/auth_google.php
@@ -0,0 +1,174 @@
+<?php
+/**
+* Database auth plug-in for phpBB3
+*
+* Authentication plug-ins is largely down to Sergey Kanareykin, our thanks to him.
+*
+* This is for authentication via the integrated user table
+*
+* @package login
+* @version $Id$
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+function oauth_exchange_code()
+{
+	global $config;
+	
+	$code = $_GET['code'];
+	$postdata = array(
+		'code' => $code,
+		'client_id' => $config['oauth_client_id'],
+		'client_secret' => $config['oauth_client_secret'],
+		'redirect_uri' => $config['server_protocol'] . $config['server_name'] . $config['script_path'] . '/oauth.php',
+		'grant_type' => 'authorization_code',
+		);
+	
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://accounts.google.com/o/oauth2/token');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	$access_token = json_decode($output)->access_token;
+	if (!$access_token)
+		return false;
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/oauth2/v2/userinfo?access_token=' . $access_token);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	$userinfo = json_decode($output);
+	
+	return $userinfo;
+}
+
+function user_row_google($userinfo)
+{
+	global $db, $config, $user;
+	$sql = 'SELECT group_id
+			FROM ' . GROUPS_TABLE . "
+			WHERE group_name = '" . $db->sql_escape('REGISTERED') . "'
+			AND group_type = " . GROUP_SPECIAL;
+	$result = $db->sql_query($sql);
+	$row = $db->sql_fetchrow($result);
+	$db->sql_freeresult($result);
+	
+	if (!$row)
+	{
+		trigger_error('NO_GROUP');
+	}
+	
+	return array(
+		'username'		=> "Google: " . $userinfo->email,
+		'user_password'	=> '',
+		'user_email'	=> $userinfo->email,
+		'group_id'		=> (int) $row['group_id'],
+		'user_type'		=> USER_NORMAL,
+		'user_ip'		=> $user->ip,
+		'user_new'		=> ($config['new_member_post_limit']) ? 1 : 0,
+		);
+}
+
+function verify_session_google()
+{
+	global $user;
+	
+	$a = json_decode(base64_decode($_GET['state']));
+	if ($a && $a->session == $user->session_id)
+	{
+		return true;
+	}
+	return false;
+}
+
+function login_google($useless_username, $useless_password)
+{
+	global $user, $db;
+	
+	if (!verify_session_google())
+	{
+		return array(
+			'status'		=> LOGIN_ERROR_EXTERNAL_AUTH,
+			'error_msg'		=> 'GOOGLE_INTERNAL_AUTH_ERROR',
+			'user_row'		=> array('user_id' => ANONYMOUS),
+			);
+	}
+
+	$userinfo = oauth_exchange_code();
+	if (!$userinfo)
+	{
+		return array(
+			'status'		=> LOGIN_ERROR_EXTERNAL_AUTH,
+			'error_msg'		=> 'GOOGLE_INTERNAL_AUTH_ERROR',
+			'user_row'		=> array('user_id' => ANONYMOUS),
+			);
+	}
+	
+	$username = utf8_clean_string("Google: " . $userinfo->email);
+	$sql = 'SELECT user_id, username, user_email, user_type, username_clean
+			FROM ' . USERS_TABLE . ' WHERE username_clean = \'' . $username . '\'';
+	$result = $db->sql_query($sql);
+	$row = $db->sql_fetchrow($result);
+	$db->sql_freeresult($result);
+	if ($row)
+	{
+		return array(
+			'status'	=> LOGIN_SUCCESS,
+			'error_msg'	=> false,
+			'user_row'	=> $row,
+			);
+	}
+	
+	return array(
+		'status'	=> LOGIN_SUCCESS_CREATE_PROFILE,
+		'error_msg'	=> false,
+		'user_row'	=> user_row_google($userinfo),
+		);
+}
+
+function auth_redirect_google($info)
+{
+	global $config, $user;
+	
+	$client_id = $config['oauth_client_id'];
+	$redirect_uri = $config['server_protocol'] . $config['server_name'] . $config['script_path'] . '/oauth.php';
+	$scope = 'https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile';
+	$response_type = 'code';
+	$append_info = array(
+		'session' => $user->session_id,
+		'info' => $info,
+		);
+	$state = base64_encode(json_encode($append_info));
+	
+	return 'https://accounts.google.com/o/oauth2/auth' . 
+			'?client_id=' . $client_id . 
+			'&response_type=' . $response_type . 
+			'&scope=' . $scope . 
+			'&redirect_uri=' . $redirect_uri . 
+			'&state=' . $state;
+}
+
+function oauth_redirect_info_google()
+{
+	if (!verify_session_google())
+		return false;
+
+	$a = json_decode(base64_decode($_GET['state']));
+	return $a->info;
+}
+
+?>
---
+++ root/forum/oauth.php
@@ -0,0 +1,39 @@
+<?php
+
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+require($phpbb_root_path . 'common.' . $phpEx);
+require($phpbb_root_path . 'includes/functions_user.' . $phpEx);
+require($phpbb_root_path . 'includes/functions_module.' . $phpEx);
+
+define('IN_LOGIN', true);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+$info = false;
+$method = trim(basename($config['auth_method']));
+include_once($phpbb_root_path . 'includes/auth/auth_' . $method . '.' . $phpEx);
+$method = 'oauth_redirect_info_' . $method;
+if (function_exists($method))
+{
+	$info = $method();
+}
+if (!$info)
+{
+	redirect('index.' . $phpEx);
+}
+
+// Hack code
+$_POST['login'] = 'Login';
+$_REQUEST['credential'] = ($info->admin) ? md5(unique_id()) : false;
+
+login_box($info ->redirect, $info->l_explain, $info->l_success, $info->admin);
+
+//require($phpbb_root_path . 'adm/index.' . $phpEx);
+//require($phpbb_root_path . 'ucp.' . $phpEx);
+
+?>
