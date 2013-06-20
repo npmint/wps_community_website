80fe3021464474d732c65506f4cd8588af29eb1e
jinchizhong <jinchizhong@kingsoft.com>
2013-06-20 16:17:41 +0800

    add friendly hyperlink, and modify code style.

--- root/parts/parts.rb
+++ root/parts/parts.rb
@@ -12,25 +12,25 @@ def html_header title
 </head>
 <body>
 <div id="header">
-	<div id="logo">
-		<a href="/"><img src="/images/logo.png" alt="Kingsoft WPS Community" /></a>
-	</div>
-	<div>
-		<div class="h_navi">
-			<span><a href="/">Home</a></span>
-			<span><a href="/download.html">Download</a></span>
-			<span><a href="/forum">Forum</a></span>
-			<span><a href="/faq.html">FAQ</a></span>
-		</div>
-	</div>
+  <div id="logo">
+    <a href="/"><img src="/images/logo.png" alt="Kingsoft WPS Community" /></a>
+  </div>
+  <div>
+    <div class="h_navi">
+      <span><a href="/">Home</a></span>
+      <span><a href="/download.html">Download</a></span>
+      <span><a href="/forum">Forum</a></span>
+      <span><a href="/faq.html">FAQ</a></span>
+    </div>
+  </div>
 </div>
 <div>
-	<br/>
-	<p class="framed">
-		<b>Tips: </b>Currently, our community is starting. We will change our pages frequently. 
-		If you meet some error(such as 404, 500), visit it after a while, please!
-		If nothing improved after some days, <a href="mailto:wps_linux@kingsoft.com">contact us</a>.
-	</p>
+  <br/>
+  <p class="framed">
+    <b>Tips: </b>Currently, our community is starting. We will change our pages frequently. 
+    If you meet some error(such as 404, 500), visit it after a while, please!
+    If nothing improved after some days, <a href="mailto:wps_linux@kingsoft.com">contact us</a>.
+  </p>
 </div>
 EOF
 end
@@ -39,7 +39,8 @@ def html_tail
 <<EOF
 <br/>
 <div id="foot"> 
-	<span>Copyright &copy; 2008-2012 Kingsoft Office Corporation, All Rights Reserved</span>
+  <span><a href="http://www.ksosoft.com">Offical Site</a> | bbb | ccc</span>
+  <span>Copyright &copy; 2008-2012 Kingsoft Office Corporation, All Rights Reserved</span>
 </div>
 </body>
 </html>
