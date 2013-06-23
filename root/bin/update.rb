6c72551bab7bfa23851d91358329c5ed7e4349e3
jinchizhong <jinchizhong@kingsoft.com>
2013-06-23 20:52:30 +0800

    add update log and add a debug script

--- root/bin/update.rb
+++ root/bin/update.rb
@@ -3,6 +3,21 @@
 puts "Content-Type: text/plain"
 puts 
 
-puts `git pull --rebase`
+gitlog = `git pull --rebase 2>&1`
 
-puts "git pull return: #{$?}"
+gitret = "git pull return: #{$?}"
+
+if not $root2
+  $root2 = "../.."
+end
+log = open($root2 + "/log/update.log", "a")
+log.puts Time.now
+log.puts "Call from #{ENV["REMOTE_ADDR"]}"
+log.puts gitlog
+log.puts gitret
+log.puts 
+log.close
+
+puts gitlog
+puts gitret
+puts
