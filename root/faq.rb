3fc180745318332836a293cea3160f8bfcf0d1c7
jinchizhong <jinchizhong@kingsoft.com>
2013-07-06 22:47:46 +0800

    faq page add anchor support.

--- root/faq.rb
+++ root/faq.rb
@@ -2,10 +2,28 @@ require 'cgi'
 require 'parts/parts.rb'
 require 'yaml'
 
+def get_ref_name q
+  r = ""
+  q.each_char do |x|
+    if x >= 'a' and x <= 'z'
+      r += x
+    elsif x >= 'A' and x <= 'Z'
+      r += x.downcase
+    elsif x >= '0' and x <= '9'
+      r += x
+    elsif x == ' ' or x == '-'
+      r += '_'
+    end
+  end
+  return r
+end
+
 def html_faq
   faqs = YAML.load_file $root2 + "/data/faqs.yaml"
   faqs.collect do |faq|
-    "<b>Q: #{faq["Q"]}</b><br/>A: #{faq["A"]}<br/><br/>"
+    "<a name=\"#{get_ref_name faq["Q"]}\"></a>
+    <b>Q: #{faq["Q"]}</b><br/>
+    A: #{faq["A"]}<br/><br/>"
   end.join "\n"
 end
 
