431a368c4ffd7a16eae52bc173fbc386777f5b90
jinchizhong <jinchizhong@kingsoft.com>
2013-07-08 17:14:05 +0800

    update main page

--- root/index.rb
+++ root/index.rb
@@ -44,6 +44,9 @@ cont = <<EOF
 <ul>
   <li><b>About Kingsoft Office:</b> Kingsoft Office ( also known as WPS Office ) is a vibrant office suite since 1989.
   We have more than 200 developers and always devote ourselves to creating comfortable, efficient and smart office experience.
+  Kingsoft Office is cross-platform commercial software. So far, we have <a href="http://www.ksosoft.com/downloads/windows.html">Windows</a> 
+  and <a href="/download.html">Linux</a> versions for desktop, <a href="http://www.ksosoft.com/downloads/android.html">Android</a>
+  and <a href="http://www.ksosoft.com/downloads/ios.html">iOS</a> versions for mobile.
   For more information, you can visit our <a href="http://www.ksosoft.com">offical website</a>, or 
   click <a href="http://www.ksosoft.com/company/index.html">here</a> to our offical website's about page, directly.</li>
   <li><b>About us:</b> We are a small branch of the Kingsoft Office team,
