778ab9712ce1f86149d5c6448246f939c07aa5d2
jinchizhong <jinchizhong@kingsoft.com>
2013-07-10 15:36:58 +0800

    modify main page

--- root/index.rb
+++ root/index.rb
@@ -42,12 +42,19 @@ cont = <<EOF
 #{html_header "Home"}
 <h1>Welcome to Kingsoft Office International Community</h1>
   <div id="slides">
-    <img src="/images/aaa.png">
-    <img src="/images/aaa.png">
-    <img src="/images/aaa.png">
-    <img src="/images/aaa.png">
+    <img src="/images/slide-wps.png">
+    <img src="/images/slide-wpp.png">
+    <img src="/images/slide-et.png">
     <a href="#" class="slidesjs-previous slidesjs-navigation"><img src="/images/left.png"></i></a>
     <a href="#" class="slidesjs-next slidesjs-navigation"><img src="/images/right.png"></i></a>
+  </div>
+  <div class="center">
+    Kingsoft Office, the simple, effective, powerful and comfortable office suite, since 1989. It had landed Linux. <br/>
+    Let us do our best to make a best Linux Office Suite.
+    For <a href="http://www.ksosoft.com/downloads/windows.html" target="_blank">Windows</a>/
+    <a href="http://www.ksosoft.com/downloads/android.html" target="_blank">Android</a>/
+    <a href="http://www.ksosoft.com/downloads/ios.html" target="_blank">iOS</a>, 
+    click <a href="http://www.ksosoft.com/" target="_blank">here</a>, please.
   </div>
   <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
   <script src="js/jquery.slides.min.js"></script>
