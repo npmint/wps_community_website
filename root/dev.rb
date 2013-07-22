0b5d15470fddb6a669ec7b41030fa79dd1f42b71
jinchizhong <jinchizhong@kingsoft.com>
2013-07-22 18:12:32 +0800

    add note

--- root/dev.rb
+++ root/dev.rb
@@ -7,6 +7,10 @@ cont = <<EOF
 <h1>Develop for KSO/WPS</h1>
   <h2>Translate KSO/WPS</h2>
     Project Address: <a href="https://github.com/wps-community/wps_i18n" target="_blank">https://github.com/wps-community/wps_i18n</a>
+    <br/><b>Note: </b>We will adjust some framework about internationalization for KSO/WPS in alpha 12.
+    So some of translation items will be lost in alpha 12(about 10-30%). 
+    Beta 2 will be released base on alpha 11. And beta 2 will be maintained for a long time.
+    If you feel worried about it, you can wait alpha 12. 
   <h2>Qt modified by KSO/WPS</h2>
     Project Address: <a href="https://gitcafe.com/wpsv9/qt-kso-integration" target="_blank">https://gitcafe.com/wpsv9/qt-kso-integration</a>
   <h2>WPS Community Code</h2>
