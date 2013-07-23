a439970b284e28d0c58c1204d812c93ad5c83e20
jinchizhong <jinchizhong@kingsoft.com>
2013-07-23 18:20:14 +0800

    add a translate progress

--- root/dev.rb
+++ root/dev.rb
@@ -1,13 +1,38 @@
 require 'cgi'
 require 'include/parts.rb'
 
+def html_mui_progress
+  fpath = $root2 + "/var/wps_mui.st"
+  if not File.exists? fpath
+    return
+  end
+  cont = ""
+  sts = []
+  open(fpath) do |fd|
+    fd.each_line do |line|
+      line.chomp!
+      c = /(\w+)\s*(\d+)\s*\/\s*(\d+)/.match line
+      sts += [ [line, c[2], c[3], c[2].to_f / c[3].to_f] ]
+    end
+  end
+  sts.sort! {|x, y| -(x[3] <=> y[3])}
+  sts.each do |c|
+    cont += "<div style='background: gray; position:relative; height: 1.2em;'>
+    <div style='background: green; width: #{c[3] * 100}%; height: 100%; position: absolute; top: 0;'></div>
+    <div style='position: absolute; font-family: Monospace;'>#{c[0]}</div>
+    </div>"
+  end
+  return cont
+end
+
 cont = <<EOF
 #{html_header "Development"}
 <div class="body">
 <h1>Develop for KSO/WPS</h1>
   <h2>Translate KSO/WPS</h2>
     Project Address: <a href="https://github.com/wps-community/wps_i18n" target="_blank">https://github.com/wps-community/wps_i18n</a>
-    <br/><b>Note: </b>We will adjust some framework about internationalization for KSO/WPS in alpha 12.
+    <br/>#{html_mui_progress}
+    <b>Note: </b>We will adjust some framework about internationalization for KSO/WPS in alpha 12.
     So some of translation items will be lost in alpha 12(about 10-30%). 
     Beta 2 will be released base on alpha 11. And beta 2 will be maintained for a long time.
     If you feel worried about it, you can wait alpha 12. 
