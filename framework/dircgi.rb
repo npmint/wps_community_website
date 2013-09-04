4b3f2a7ac991fa96b5734345b2fe124c2586d331
so.zengtao <so.zengtao@gmail.com>
2013-09-04 15:35:28 +0800

    add path under download

--- framework/dircgi.rb
+++ framework/dircgi.rb
@@ -4,12 +4,23 @@ require 'cgi'
 require '../root/include/parts.rb'
 
 $dirpath = ENV['DOCUMENT_ROOT'] + ENV['DOCUMENT_URI']
-$filepath = ENV['DOCUMENT_URI'] 
+$filepath = ENV['DOCUMENT_URI']
+$home = ENV['DOCUMENT_ROOT']
 def html_readme
   cont = ""
   if File.exist?($dirpath + "/README.md")
     cont = `markdown #{$dirpath}"/README.md" 2>&1`
   end
+  return cont
+end
+
+def html_path
+  cont = "<h1>\> <a href=\"..\">HomePage </a> \> <a href=\".\">download</a>"
+  if ("/download/" != $filepath)
+    left = $filepath.gsub("/download/","")
+    cont += " \> <a href=\"./#{left}\">#{left}</a>"
+  end
+  cont += "</h1>"
   return cont
 end
 
@@ -19,7 +30,6 @@ def html_sub_dir
   cont += "<div class=\"mui_item\">"
   cont += "<table summary=\"Directory Listing\" cellpadding=\"0\" cellspacing=\"10\" border=\"0\" width=\"1000\">"
   cont += "<thread><tr><td class=\"n\" style=\"font-size:18px;\"><b>Name</b></td><td class=\"m\" style=\"font-size:18px;\"><b>Last Modified Time</b></td></tr></thread>"
-  cont += "<tr><td class=\"n\"><a href=\"#{$filepath + "/.." }/\"> Parent Directory</a></td></tr>"
   x.collect do |a|
     if (a != '.' && a != '..' && a != 'README.md')
       if (FileTest.directory?($dirpath + "/" +a))
@@ -37,6 +47,8 @@ end
 
 cont = <<EOF
 #{html_header "Development"}
+
+#{html_path}
 
 #{html_readme}
 <p>
