caddc57568fc49f9ea45edc3c7a9295480b93f89
so.zengtao <so.zengtao@gmail.com>
2013-09-04 14:21:14 +0800

    fix error 404 while include /donwload/

--- framework/dircgi.rb
+++ framework/dircgi.rb
@@ -19,7 +19,7 @@ def html_sub_dir
   cont += "<div class=\"mui_item\">"
   cont += "<table summary=\"Directory Listing\" cellpadding=\"0\" cellspacing=\"10\" border=\"0\" width=\"1000\">"
   cont += "<thread><tr><td class=\"n\" style=\"font-size:18px;\"><b>Name</b></td><td class=\"m\" style=\"font-size:18px;\"><b>Last Modified Time</b></td></tr></thread>"
-  cont += "<tr><td class=\"n\"><a href=\"./\"> Parent Directory</a></td></tr>"
+  cont += "<tr><td class=\"n\"><a href=\"#{$filepath + "/.." }/\"> Parent Directory</a></td></tr>"
   x.collect do |a|
     if (a != '.' && a != '..' && a != 'README.md')
       if (FileTest.directory?($dirpath + "/" +a))
