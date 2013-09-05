f46fa0a0ff6ae091f4976a467fedabf459a92595
jinchizhong <jinchizhong@kingsoft.com>
2013-09-05 20:40:19 +0800

    fixup st.rb

--- root/bin/st.rb
+++ root/bin/st.rb
@@ -3,7 +3,7 @@
 require 'cgi'
 cgi = CGI.new
 
-open(ENV["DOCUMENT_ROOT"] + "../log/statistics.log", "a") do |f|
+open(ENV["DOCUMENT_ROOT"] + "/../log/statistics.log", "a") do |f|
   f.puts Time.now.strftime("%F %T %z") + " " + cgi["t"] + " " + cgi["a"]
 end
 
