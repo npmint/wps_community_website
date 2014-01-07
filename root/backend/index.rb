82bc594793861b65958ef868e5fc3b369a34512f
jinchizhong <jinchizhong@kingsoft.com>
2014-01-07 13:40:48 +0800

    update backend page

--- root/backend/index.rb
+++ root/backend/index.rb
@@ -1,8 +1,13 @@
 require 'libraries/auth'
+require 'template/overall'
 require 'cgi'
 
 ensure_backend_access
 
 CGI.new.out {
-  "Nothing here now."
+  <<EOF
+  #{html_header "BACKEND"}
+  <h1>Nothing here now</h1>
+  #{html_tail}
+EOF
 }
