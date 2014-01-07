ed168cc84907ec218b3c04e68d830629102173a9
jinchizhong <jinchizhong@kingsoft.com>
2014-01-07 11:10:07 +0800

    add sha1 function to dirlist

--- framework/dircgi.rb
+++ framework/dircgi.rb
@@ -3,11 +3,14 @@
 require "#{File.dirname(__FILE__)}/libraries/dirs.rb"
 require 'cgi'
 require 'template/overall.rb'
+require 'digest/sha1'
 
 $dirpath = ENV['DOCUMENT_ROOT'] + ENV['REQUEST_URI']
 $dirpath.gsub!('//', '/')
+$dirpath.gsub!(/\/$/, '')
 $filepath = ENV['REQUEST_URI']
 $home = ENV['DOCUMENT_ROOT']
+$varpath = ENV['DOCUMENT_ROOT'] + "/../var"
 
 def html_readme
   cont = ""
@@ -43,20 +46,36 @@ def humen_readable_size file_path
   end
 end
 
+def cached_sha1 file_path
+  path_digest = Digest::SHA1.hexdigest file_path
+  digest_file_path = $varpath + "/sha1/#{path_digest}.path"
+  if File.exists? digest_file_path
+    sha1 = File.read digest_file_path
+    return sha1
+  else
+    sha1 = Digest::SHA1.file(file_path).hexdigest
+    Dir.mkdir $varpath + "/sha1" if !Dir.exists? $varpath + "/sha1"
+    File.write(digest_file_path, sha1)
+    return sha1
+  end
+end
+
 def html_sub_dir
   cont = "<div class='file_list'><table style='width:100%;'>"
-  cont += "<thead><tr><th class='file_name'>File Name</th><th class='file_size'>File Size</th><th class='file_time'>Modify Time (UTC)</th></tr></thead>"
+  cont += "<thead><tr><th class='file_name'>File Name</th><th class='file_size'>File Size</th><th class='file_time'>Modify Time (UTC)</th><th class='file_sha1'>SHA1</th></tr></thead>"
   x = Dir.entries($dirpath).each.to_a.sort do |a, b| a.to_s <=> b.to_s end
   x.each do |a|
     if (a != '.' && a != 'README.md')
       if (FileTest.directory?($dirpath + "/" +a))
         cont += "<tr><td class='file_name'><a href=\"./#{a}/\">#{a}</a>/</td>
                      <td class='file_size'>-</td>
-                     <td class='file_time'>#{File.mtime($dirpath + "/" +a).getutc.strftime "%F %T"}</td></tr>"
+                     <td class='file_time'>#{File.mtime($dirpath + "/" +a).getutc.strftime "%F %T"}</td>
+                     <td class='file_sha1'>-</td></tr>"
       else 
         cont += "<tr><td class='file_name'><a href=\"./#{a}\">#{a}</a></td>
                      <td class='file_size'>#{humen_readable_size($dirpath + "/" +a)}</td>
-                     <td class='file_time'>#{File.mtime($dirpath + "/" +a).getutc.strftime "%F %T"}</td></tr>"
+                     <td class='file_time'>#{File.mtime($dirpath + "/" +a).getutc.strftime "%F %T"}</td>
+                     <td class='file_sha1'>#{cached_sha1($dirpath + "/" + a)}</td></tr>"
       end
     end
   end
