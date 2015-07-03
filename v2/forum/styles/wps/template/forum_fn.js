e12d7d86c422985b9c68c10d363c8543f623839c
jinchizhong <jinchizhong@kingsoft.com>
2015-07-03 11:37:57 +0800

    use comboot as base style

--- v2/forum/styles/wps/template/forum_fn.js
+++ v2/forum/styles/wps/template/forum_fn.js
@@ -139,7 +139,7 @@ function subPanels(p)
 				e.style.display = 'block';
 				if (t)
 				{
-					t.className = 'activetab';
+					t.className = 'btn btn-primary disabled';
 				}
 			}
 			else
@@ -147,7 +147,7 @@ function subPanels(p)
 				e.style.display = 'none';
 				if (t)
 				{
-					t.className = '';
+					t.className = 'btn btn-default';
 				}
 			}
 		}
@@ -403,7 +403,7 @@ function apply_onkeypress_event()
 	// jQuery code in case jQuery is used
 	if (jquery_present)
 	{
-		jQuery('form input[type=text], form input[type=password]').live('keypress', function (e)
+		jQuery('form input[type=text], form input[type=password]').on('keypress', function (e)
 		{
 			var default_button = jQuery(this).parents('form').find('input[type=submit].default-submit-action');
 			
