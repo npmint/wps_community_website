7262bf5b708b93163d51fa4db5b01867a82789c9
jinchizhong <jinchizhong@kingsoft.com>
2015-08-17 23:18:12 +0800

    add image to news

--- v2/app/models/news.js
+++ v2/app/models/news.js
@@ -3,6 +3,7 @@ var config = require('../../config');
 var NEWS_DIR = config.root.repo + '/data/news';
 
 var fs = require('fs');
+var jsyaml = require('js-yaml');
 
 var cache = [];
 
@@ -14,6 +15,10 @@ exports.prepare = function(done) {
       var parts = cont.split(/====+\n/);
       var body = '<p>' + parts[1].split(/\n\n+/).join('</p><p>') + '</p>';
       cache.unshift({title: parts[0], body: body});
+    } else if (fname.match(/\.yaml$/)) {
+      var x = jsyaml.load(fs.readFileSync(NEWS_DIR + '/' + fname));
+      x.body = '<p>' + x.body.split(/\n\n+/).join('</p><p>') + '</p>';
+      cache.unshift(x);
     }
   });
 };
